c94c8f0c8f434e3486ac9de35b74176eef698049
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-07 20:16:42 +0100

    add comment 📚

--- parser.js
+++ parser.js
@@ -1,3 +1,6 @@
+// we get a key and value, but key kan be joined
+// by dots that need to be nested inside the object
+// example: a.b and c should return this object => {a: {b: c}}
 const parse = (dottedKey = "", value = "", object = {}) => {
   dottedKey.split(".").reduce((acc, v, index) => {
     return index + 1 === dottedKey.split(".").length
